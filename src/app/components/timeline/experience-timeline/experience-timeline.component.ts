80a25e8867b481e33f1609d0e2395064606cce8b
arushi262 <arushithakur262@gmail.com>
2023-05-28 12:44:13 -0400

    mail this api

--- src/app/components/timeline/experience-timeline/experience-timeline.component.ts
+++ src/app/components/timeline/experience-timeline/experience-timeline.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-experience-timeline',
   templateUrl: './experience-timeline.component.html',
-  styleUrls: ['./experience-timeline.component.scss']
+  styleUrls: ['./experience-timeline.component.scss'],
 })
-export class ExperienceTimelineComponent {
-
-}
+export class ExperienceTimelineComponent {}
